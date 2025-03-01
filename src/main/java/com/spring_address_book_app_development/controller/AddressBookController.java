a1aebeb05965b43fe38ec02544b0816c839fbad0
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-03-01 12:11:21 +0530

    [Kuldeep] Add. Create a Rest Controller to demonstrate the various HTTP Methods

--- src/main/java/com/spring_address_book_app_development/controller/AddressBookController.java
+++ src/main/java/com/spring_address_book_app_development/controller/AddressBookController.java
@@ -1,5 +1,7 @@
 package com.spring_address_book_app_development.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,22 +9,27 @@ import org.springframework.web.bind.annotation.*;
 public class AddressBookController {
 
     @GetMapping("/get")
-    public String getAddressBook() {
-        return "addressBook";
+    public ResponseEntity<String> getAddressBook() {
+        return new ResponseEntity<> ("addressBook", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getAddressBookById(@PathVariable int id) {
+        return new ResponseEntity<> ("addressBook", HttpStatus.OK);
     }
 
     @PostMapping("/post")
-    public String postAddressBook() {
-        return "addressBook";
+    public ResponseEntity<String> postAddressBook(@RequestBody String address) {
+        return new ResponseEntity<> ("addressBook", HttpStatus.OK);
     }
 
-    @PutMapping("/put")
-    public String putAddressBook() {
-        return "addressBook";
+    @PutMapping("/put/{id}")
+    public ResponseEntity<String> putAddressBook(@PathVariable int id) {
+        return new ResponseEntity<> ("addressBook", HttpStatus.OK);
     }
 
     @DeleteMapping("/delete")
-    public String deleteAddressBook() {
-        return "addressBook";
+    public ResponseEntity<String> deleteAddressBook() {
+        return new ResponseEntity<> ("addressBook", HttpStatus.OK);
     }
 }
